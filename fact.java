011072bd8c0327e86b3378cab3b8637ee352abaf
P J Sreedeep <95912047+sreedeep1999@users.noreply.github.com>
2022-03-22 15:37:39 +0530

    Add files via upload

---
+++ fact.java
@@ -0,0 +1,18 @@
+public class fact
+{
+public static void main(String args[])
+{
+int n=5;
+
+int i,fact=1;
+
+for(i=1;i<=n;i++)
+
+{
+
+fact=fact*i;
+}
+System.out.println("factorial of"+n+"="+fact);
+}
+}
+ 
---
+++ sumz.java
@@ -0,0 +1,9 @@
+public class sumz
+{
+public static void main(String args[])
+{
+int a=30 , b=20 ,c;
+c=a+b;
+System.out.println("sum="+c);
+}
+}
