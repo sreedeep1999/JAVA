a6854af37d3f4025dfe878563bd07022191899e6
P J Sreedeep <95912047+sreedeep1999@users.noreply.github.com>
2022-04-26 13:01:56 +0530

    Update matrix.java

--- matrix.java
+++ matrix.java
@@ -1,63 +1,83 @@
-import java.util.*;
-public class matrixAdd
+ import java.util.Scanner;
+class matrixadd
 {
-public static void main(String[] args)
+public static void main(String args[])
 {
-int p,q,m,n;
-Scanner s=new Scanner(System.in);
-System.out.println("Enter the no of rows in the first matrix:");
-p=s.nextInt();
-System.out.println("Enter the number of colomn in the first matrix:");
-q=s.nextInt();
-System.out.println("Enter the no of rows in the second matrix:");
-m=s.nextInt();
-System.out.println("Enter the no of colomns in the second matrix:");
-n=s.nextInt();
+int p,q,m,n,i,j,k;
+Scanner S=new Scanner(System.in);
+System.out.println("enter the number of rows of first matrix");
+p=S.nextInt();
+System.out.println("enter the number of columns of first matrix");
+q=S.nextInt();
+System.out.println("enter the number of rows of second matrix");
+m=S.nextInt();
+System.out.println("enter the number of columns of first matrix");
+n=S.nextInt();
 if(p==m&&q==n)
 {
-        int a[][]=new int[p][q];
-        int b[][]=new int[m][n];
-        int c[][]=new int[m][n];
-        System.out.println("Enter the elements of first matrix:");
-        for(int i=0;i<p;i++)
-        for(int j=0;j<n;j++)
-                a[i][j]=s.nextInt();
-        System.out.println("Enter the no elements in the second matrix:");
-        for(int i=0;i<m;i++)
-        for(int j=0;j<n;j++)
-        b[i][j]=s.nextInt();
-        System.out.println("First matrix:");
-        for(int i=0;i<p;i++)
-        {
-        for(int j=0;j<q;j++)
-        System.out.print(a[i][j]+"");
-        System.out.println("");
-        }
-        System.out.println("second matrix:");
-        for(int i=0;i<m;i++) 
-        {
-        for(int j=0;j<n;j++)
-        {
-        System.out.print(b[i][j] + "");
-         }
-System.out.print("");
+int a[][]=new int[p][q];
+int b[][]=new int[m][n];
+int c[][]=new int[m][n];
+
+System.out.println("enter all the elements of first matrix");
+for(i=0;i<p;i++)
+for(j=0;j<q;j++)
+a[i][j]=S.nextInt();
+
+System.out.println("enter all the elements of second matrix");
+for(i=0;i<m;i++)
+for(j=0;j<n;j++)
+b[i][j]=S.nextInt();
+
+System.out.println("first matrix");
+
+for(i=0;i<p;i++)
+{
+for(j=0;j<q;j++)
+{
+System.out.println(a[i][j]+"");
+System.out.println("");
+}
 }
 
-for(int i=0;i<p;i++)
-for(int j=0;j<n;j++)
-for(int k=0;k<q;k++)
+
+System.out.println("second matrix");
+
+for(i=0;i<m;i++)
+{
+for(j=0;j<n;j++)
+{
+System.out.println(b[i][j]+"");
+System.out.println("");
+}
+}
+
+for(i=0;i<p;i++)
+for(j=0;j<n;j++)
+for(k=0;k<q;k++)
 c[i][j]=a[i][j]+b[i][j];
 System.out.println("matrix after addition");
-for(int i=0;i<p;i++)
+for(i=0;i<p;i++)
 {
-for(int j=0;j<n;j++)
-System.out.print(c[i][j]+"");
-System.out.print("");
+for(j=0;j<n;j++)
+System.out.println(c[i][j]+"");
+System.out.println("");
 }
 }
 else
 {
-System.out.println("addition is not possible");
+System.out.println(" addition notpossible");
 }
 }
 }
+
+
+
+
+
+
+
+
+
+
+
