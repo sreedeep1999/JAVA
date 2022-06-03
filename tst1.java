061c753e8a73092d677b3563833b925a5c7b799c
P J Sreedeep <95912047+sreedeep1999@users.noreply.github.com>
2022-06-03 15:08:14 +0530

    Update tst1.java

--- tst1.java
+++ tst1.java
@@ -4,13 +4,11 @@ class Test extends Applet
 {
 public void paint(Graphics g)
 {
-public void init()
-{
 g.drawLine(10,20,10,30);
 g.drawString("welcome",10,10);
 }
 }
-}
+
 /*
 <applet code="Test.applet" width=500 height=500>
 </applet>
