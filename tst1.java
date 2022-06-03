bffbbf8ee567f236d05e175798956cba02a5c2e2
P J Sreedeep <95912047+sreedeep1999@users.noreply.github.com>
2022-06-03 15:45:59 +0530

    Update tst1.java

--- tst1.java
+++ tst1.java
@@ -1,15 +1,25 @@
 import java.applet.*;
 import java.awt.*;
-class Test extends Applet
+public class Test1 extends Applet
 {
 public void paint(Graphics g)
 {
+g.setColor(Color.yellow);
+
+g.fillOval(80,70,150,150);
+
+g.setColor(Color.red);
+g.fillOval(120,120,15,15);
+g.fillOval(170,120,15,15);
+ g.fillArc(130,150,45,45,180,180);
+ 
 g.drawLine(10,20,10,30);
 g.drawString("welcome",10,10);
+g.drawRect(10,30,60,40);
+ 
 }
 }
-
 /*
-<applet code="Test.applet" width=500 height=500>
+<applet code="Test1.java" width=500 height=500>
 </applet>
 */
