f37769b947e707c4c41589d2d840b10aa4868017
P J Sreedeep <95912047+sreedeep1999@users.noreply.github.com>
2022-06-03 14:52:06 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,13 @@
+import util.Applet.*;
+import java.awt.*;
+/*
+<applet code="Test.applet" width=500 height=500>
+</applet>
+*/
+class Test extends Applet
+{
+public void paint(Graphics g)
+{
+g.drawLine(10,20,10,30);
+}
+}
